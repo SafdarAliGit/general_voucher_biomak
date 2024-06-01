2faccc429006e71ae91706bc90be87d2345edf21
safdar211ali <safdar211@hotmail.com>
2024-06-01 16:57:08 +0500

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## General Voucher Biomak
+
+general_voucher_biomak
+
+#### License
+
+MIT
---
+++ general_voucher_biomak/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "General Voucher Biomak",
+			"type": "module",
+			"label": _("General Voucher Biomak")
+		}
+	]
